e5611e8827282d528f5fd2a307d43a4e8a081fd7
Blake McFarlane <blakescapital@gmail.com>
2024-01-04 21:13:52 -0500

    github auth

---
+++ amplify/backend/function/getAccessTokenLambda/src/index.js
@@ -0,0 +1,40 @@
+const fetch = require('node-fetch');
+
+const CLIENT_ID = "aab216eddd2d26ddfc43";
+const CLIENT_SECRET = "2a8b41295cceb3958c458b4934b9a9bc771a76ac";
+
+exports.handler = async (event) => {
+    console.log(`EVENT: ${JSON.stringify(event)}`);
+
+    const code = event.queryStringParameters.code;
+    console.log("TOKEN: " + code);
+
+    const params = "?client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&code=" + code;
+
+    try {
+        const response = await fetch("https://github.com/login/oauth/access_token" + params, {
+            method: "POST",
+            headers: {
+                "Accept": "application/json"
+            }
+        });
+        const data = await response.json();
+        console.log(data);
+
+        return {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*", // Enable CORS
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        };
+    } catch (error) {
+        console.error(error);
+
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: "Internal server error" })
+        };
+    }
+};
--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,18 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "getAccessToken": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "getAccessTokenLambda": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
